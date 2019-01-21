04b77b851a70478e915c1b23704cf8aa7049e022
Marie Burns <mcburns2222@gmail.com>
2019-01-21 01:29:55 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,6 +23,11 @@ class Scraper
     binding.pry
     #profile.css('div.social-icon-container a).collect { |
     
+    student = {}
+    profile.css('div.social-icon-container a').each do |link|
+      if link.include?('twitter') student[:twitter] = link 
+      if link.include?('linkedin') student[:linkedin] = link
+    end
     
     student = {
     :linkedin => profile.css('div.social-icon-container a')[1]['href'], :github => profile.css('div.social-icon-container a')[2]['href'], :blog => profile.css('div.social-icon-container a')[3]['href'], :twitter => profile.css('div.social-icon-container a')[0]['href'], :profile_quote => profile.css(".profile-quote").text, :bio => profile.css("p").text,
