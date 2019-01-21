44288afef5bdcd2753e83920550bda8cfb3c7eab
Marie Burns <mcburns2222@gmail.com>
2019-01-21 02:48:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,7 @@ class Student
 
   def self.create_from_collection(students_array)
     students_array.each do |student|
-      student_hash.New(student)
+      student.name.New(student)
     end
   end
 
