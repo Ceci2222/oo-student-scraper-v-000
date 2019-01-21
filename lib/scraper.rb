4e1bcee8e81a8e25b6b6d7528e43295fa1d99c59
Marie Burns <mcburns2222@gmail.com>
2019-01-21 01:23:13 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,14 +1,32 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
+
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    students = []
+    doc = Nokogiri::HTML(open('./fixtures/student-site/index.html'))
+   
+    doc.css(".student-card").each do |student|
+    s = {
+      :name => student.css("h4").text, :location => student.css('p').text, :profile_url => student.css('a').attribute("href").value
+    }
+    students << s
+    end
+    students
   end
 
   def self.scrape_profile_page(profile_url)
+    profile = Nokogiri::HTML(open(profile_url))
+    bindin.pry
+    #profile.css('div.social-icon-container a).collect { |
     
+    
+    student = {
+    :linkedin => profile.css('div.social-icon-container a')[1]['href'], :github => profile.css('div.social-icon-container a')[2]['href'], :blog => profile.css('div.social-icon-container a')[3]['href'], :twitter => profile.css('div.social-icon-container a')[0]['href'], :profile_quote => profile.css(".profile-quote").text, :bio => profile.css("p").text,
+    }
   end
 
 end
